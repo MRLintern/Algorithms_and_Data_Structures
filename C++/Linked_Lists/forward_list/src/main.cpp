5130542e3d22a465d549ea20ba735683fb9fdca6
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2024-01-13 16:21:22 +0000

    Updated main.cpp

--- C++/Linked_Lists/forward_list/src/main.cpp
+++ C++/Linked_Lists/forward_list/src/main.cpp
@@ -1,1 +1,18 @@
+// program for single linked list using forward_list
 
+#include <iostream>
+#include <forward_list>
+
+int main() {
+
+	// create linked list
+
+	const std::forward_list<double> list {3.14, 2.72, 0, 42, -1};
+
+	// iterate through the list to show values
+
+	for (const auto value : list) {
+
+		std::cout<<value<<"\n";
+	}
+}
