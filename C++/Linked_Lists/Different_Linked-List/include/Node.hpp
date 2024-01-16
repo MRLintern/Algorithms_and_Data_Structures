3891f19ddefb3d3b6212dea752c9a41d5d50bc66
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2024-01-16 18:32:20 +0000

    Updated

--- C++/Linked_Lists/Different_Linked-List/include/Node.hpp
+++ C++/Linked_Lists/Different_Linked-List/include/Node.hpp
@@ -1,1 +1,13 @@
+// node struct to represent elements of the linked list
 
+template<typename T>
+
+struct Node {
+
+	T data;
+	Node* next;
+
+	// constructor
+	Node(T value) : data(value), next(nullptr) {}
+	
+};
